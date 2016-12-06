290fc316069d4e89f2063a87e7bb34ed1e371e2e
Bamby <bamby.gassama@gmail.com>
2016-12-06 15:37:44 +0100

    Scroll reveal USP's

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -6,7 +6,7 @@ $(document). ready (function(){
   
   $(window).on("scroll",function() {
     if ( $(this).scrollTop() > 640){
-      $(".navbar").css("background-color", "##222222");
+      $(".navbar").css("background-color", "#222222");
   } else{
     $(".navbar").css("background-color", "transparent");
   }
@@ -14,16 +14,18 @@ $(document). ready (function(){
  });
 });
 
-$('a[href*="#"]:not([href="#"])').click(function() {
-  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    if (target.length) {
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000);
-      return false;
-    }
-  }
-});
 
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
